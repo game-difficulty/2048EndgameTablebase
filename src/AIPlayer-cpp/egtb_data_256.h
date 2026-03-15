df142e3e0df4c54dcb10f4f315b02862f24718fc
game-difficulty <assweeass@gmail.com>
2026-03-15 23:49:00 +0800

    Delete src/AIPlayer-cpp directory

--- src/AIPlayer-cpp/egtb_data_256.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <cstdint>
-
-extern const uint32_t EGTB256_B[60];
-extern const uint32_t EGTB256_L[60];
-extern const uint16_t* const EGTB256_SEEDS[60];
-extern const uint8_t* const EGTB256_SIGS[60];
-extern const uint16_t* const EGTB256_RATES[60];
