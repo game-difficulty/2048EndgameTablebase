052cf265a162b303650076521d0bb5385dbcfb25
game-difficulty <assweeass@gmail.com>
2026-08-22 21:31:20 +0800

    Add remote 4431 1024 tablebase

--- backend/quota/config.py
+++ backend/quota/config.py
@@ -42,6 +42,7 @@ def table_multiplier_config() -> dict[str, Any]:
                 {"prefix": "free10", "multiplier": 8},
                 {"prefix": "free11", "multiplier": 50},
                 {"prefix": "4421", "multiplier": 5},
+                {"prefix": "4431", "multiplier": 8},
                 {"prefix": "444", "multiplier": 5},
                 {"prefix": "LL", "multiplier": 8},
             ],
